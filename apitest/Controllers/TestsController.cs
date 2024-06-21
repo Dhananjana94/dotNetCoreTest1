0bfae84ce0561087842103a28413d49893c5a1ef
Dhananjana94 <nadunwanninayaka@gmail.com>
2024-06-21 12:53:41 +0530

    Created action methods

---
+++ apitest/Controllers/TestsController.cs
@@ -0,0 +1,38 @@
+﻿ using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apitest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //class level attribute
+    public class TestsController : ControllerBase
+     {
+        //action method in api
+        [HttpGet]// method level attribute
+        public IActionResult tasks()
+        {
+            //IActionResault can use to return resault as well as status code
+
+            var test = new string[] { "tst1", "tst2", "tst3" };
+            return Ok(test);
+        }
+
+        [HttpPost]
+        public IActionResult TaskPost()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult TaskDelete()
+        {
+            var Badresault = true;  
+            if (Badresault)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
